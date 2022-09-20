cef65671099960394dbc819c9feee2b887f81371
Juan Francisco Alvero <macaronij@gmail.com>
2022-09-19 23:23:48 -0300

    mock de tareas

---
+++ src/app/components/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    id?: number, //el signo de interrogacion por las dudas que no lo tiene
+    text:string,
+    day:string,
+    reminder: boolean
+}
---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: "Terminar primer modulo de Angular",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id:2,
+        text: "Ver a Naty",
+        day: "Agosto 10 a las 14:00",
+        reminder: true
+    },
+    {
+        id:3,
+        text: "Analisis glucosa",
+        day: "Agosto 12 a las 19:00",
+        reminder: false
+    },
+    {
+        id:4,
+        text: "Practicar curso",
+        day: "Agosto 13 a las 10:00",
+        reminder: true
+    }
+]
